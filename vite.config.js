e4397b2d9b08f3368a20f9bdd0cd17e41719d5fb
Casiano <luis.benitez.ayala@gmail.com>
2024-11-10 13:46:07 -0300

    Preparando despliegue a GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import ViteGitHubPages from 'vite-plugin-gh-pages'; // Asegúrate de importar correctamente el plugin
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-todo/', // Cambia esto para que coincida con el nombre de tu repositorio
-  plugins: [
-    vue(),
-    ViteGitHubPages(), // Usa el plugin de GitHub Pages correctamente
-  ],
+  base: '/vite-todo/',
+  plugins: [vue()],
 });
