83a43aa2368eff4218814a8c367e751adbf52887
Casiano <luis.benitez.ayala@gmail.com>
2024-11-10 14:01:56 -0300

    configuracion ghpages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-todo/',
-  plugins: [vue()],
+  plugins: [vue(), ghPages()],
+  base: 'vite-todo'
 });
